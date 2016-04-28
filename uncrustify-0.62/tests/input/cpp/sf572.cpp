59986182b47eb0e27097c2a61f21b33a17b2aa37
Johan <johangas@kth.se>
2016-04-28 11:52:35 +0200

    silly computer needs reinstall

---
+++ uncrustify-0.62/tests/input/cpp/sf572.cpp
@@ -0,0 +1,17 @@
+int foo()
+{
+if (1) {
+if (1) {
+if (1) {
+if ((bar1()) &&
+(bar2() ||
+(bar2() <=
+baz()))) {
+return 0;
+}
+}
+}
+}
+return 1;
+}
+
---
+++ uncrustify-0.62/tests/output/oc/50512-gh125.m
@@ -0,0 +1,19 @@
+
+[[NSFileManager defaultManager] createFileAtPath: path contents: data attributes: nil]
+
+[[NSFileManager defaultManager] createFileAtPath: path
+ contents: data
+ attributes: nil]
+
+[NSFileManager.defaultManager
+ createFileAtPath: path
+ contents: data
+ attributes: nil]
+
+[self.myProperty setObject: obj forKey: key];
+
+[myProperty setObject: obj forKey: key];
+
+NSString *newValue = [@"my string" stringByTrimmingCharactersInSet: NSCharacterSet.whitespaceCharacterSet];
+
+NSString *newValue = [myStrings[0] stringByTrimmingCharactersInSet: NSCharacterSet.whitespaceCharacterSet];
