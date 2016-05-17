7984878e6355d1e6598f80af8b38e435b15a76dc
johangas <johangas@kth.se>
2016-05-17 11:01:48 +0200

    just backing up

--- johan/setup.sh
+++ johan/setup.sh
@@ -1,15 +1,44 @@
+#compile and flash
+
+if($WITH_DTLS) then
+  cd dtls-authority
+else 
+  cd authority
+fi
+make authority.flash TARGET=nrf52dk NRF52_JLINK_SN= #TODO
+
+cd ..
+
+if($WITH_DTLS) then
+  cd dtls-keypad
+else
+  cd keypad
+fi
+
+make keypad.flash TARGET=nrf52dk ADDR=2001:db8::225:40ff:fef0:8bf0 NRF52_JLINK_SN= #TODO
+
+cd ..
+
 #setup bluetooth driver
-mount -t debugfs none /sys/kernel/debug
+sudo mount -t debugfs none /sys/kernel/debug
 
-modprobe bluetooth_6lowpan
+sudo modprobe bluetooth_6lowpan
 
-echo 35 > /sys/kernel/debug/bluetooth/6lowpan_psm
+sudo echo 35 > /sys/kernel/debug/bluetooth/6lowpan_psm
+
+#Connect
+sudo echo "connect 00:00:F7:D1:AB:65 1" > /sys/kernel/debug/bluetooth/6lowpan_control
+#TODO wait needed?
+sleep 3s
+sudo echo "connect 00:25:40:F0:8B:F0 1" > /sys/kernel/debug/bluetooth/6lowpan_control
 
 #forwarding
-echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
-sysctl -w net.ipv6.conf.all.forwarding=1
+sudo echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
+sudo sysctl -w net.ipv6.conf.all.forwarding=1
 
 #restartd routing daemon
-service radvd restart
+sudo service radvd restart
 
-ifconfig bt0 add 2001:db8::1/64
+sudo ifconfig bt0 add 2001:db8::1/64
+sudo ifconfig bt1 add 2001:db8::1/64
+sudo ifconfig bt2 add 2001:db8::1/64
